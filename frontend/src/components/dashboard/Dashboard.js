b2d73f6c8d37d1abb3f834c24b28bb597dc0e5a6
Rohit Gupta <124511104+Rohit171020@users.noreply.github.com>
2023-08-19 13:26:43 +0530

    Dashboard.js

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -1,71 +1,35 @@
 import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
-import PlanCard from "../plan-card/PlanCard";
 import { AppContext } from "../../utils/AppContext";
+import PlanCard from "../plan-card/PlanCard";
 import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
-  const {
-    logout,
-    user,
-    fetchAllSubscriptions,
-    transactions
-  } = useContext(AppContext);
+  const { logout, user, fetchAllSubscriptions, transactions } = useContext(AppContext);
   const navigator = useNavigate();
 
+  
   useEffect(() => {
     fetchAllSubscriptions();
-  }, []);
-
+  }, []); 
   const handleLogout = () => {
     localStorage.removeItem("rememberedUser");
     navigator("/");
-  };
-
-  const renderTransactionList = () => {
-    if (transactions) {
-      return (
-        <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", gap: "20px" }}>
-          {transactions.map((item) => (
-            <PlanCard
-              subId={item.subscriptionId}
-              key={item._id}
-              period={item.planDetails.period}
-              type={item.active ? "active" : null}
-              plan={item.planDetails.plan}
-              amount={
-                item.planDetails.period === "month"
-                  ? item.planDetails.planInfo["Monthly-Price"]
-                  : item.planDetails.planInfo["Yearly-Price"]
-              }
-              data={item}
-            />
-          ))}
-        </div>
-      );
-    } else {
-      return (
-        <h2 style={{ textAlign: "center", color: "white", marginTop: "40px" }}>
-          Please Wait, We are Fetching Data....
-        </h2>
-      );
-    }
+    
   };
 
   return (
     <div style={{ width: "100%" }}>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Welcome {user && user.name}
-      </h1>
+      <h1 style={{ color: "white", textAlign: "center" }}> Welcome {user && user.name}</h1>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <button
-          onClick={handleLogout}
+          onClick={handleLogout} 
           className="btn"
           style={{
             backgroundColor: "white",
             color: "#1e4c91",
             width: "inherit",
-            margin: "11px"
+            margin: "10px",
           }}
         >
           Log Out
@@ -75,18 +39,39 @@ export default function Dashboard() {
           className="btn"
           style={{
             backgroundColor: "white",
-            color: "#1e4c92",
-            width: "inherit"
+            color: "#1e4c91",
+            width: "inherit",
           }}
         >
           Browse Plans
         </Link>
       </div>
 
-      <div style={{ marginTop: 22 }}>
-        {renderTransactionList()}
+      <div style={{ marginTop: 20 }}>
+        {transactions ? (
+          <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", gap: "20px" }}>
+            {transactions.map((item) => (
+              <PlanCard
+                subId={item.subscriptionId}
+                key={item._id}
+                period={item.planDetails.period}
+                type={item.active ? "active" : null}
+                plan={item.planDetails.plan}
+                amount={
+                  item.planDetails.period === "month"
+                    ? item.planDetails.planInfo["Monthly-Price"]
+                    : item.planDetails.planInfo["Yearly-Price"]
+                }
+                data={item}
+              />
+            ))}
+          </div>
+        ) : (
+          <h2 style={{ textAlign: "center", color: "white", marginTop: "40px" }}>
+            Please Wait, We are Fetching Data....
+          </h2>
+        )}
       </div>
     </div>
   );
 }
-
