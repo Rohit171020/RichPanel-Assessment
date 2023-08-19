04fd8a515ed62b8bda21f04b02a289c1a91ed5f4
Rohit Gupta <124511104+Rohit171020@users.noreply.github.com>
2023-08-19 13:24:48 +0530

    StripeContainer.js

--- frontend/src/components/stripe/StripeContainer.js
+++ frontend/src/components/stripe/StripeContainer.js
@@ -2,8 +2,8 @@ import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 import PaymentCard from '../payment-card/PaymentCard';
 
-const PUBLIC_KEY =
-"pk_test_51LSvzlSDr67aca5VWkvF5mWBipmZ7q8Vq1dXxV2czrPJ7LzWQYEmVJXVCCER6wWahzkI0Xx8en09nvre4pgcqLQe00m6XOjPxq"
+const PUBLIC_KEY = "pk_test_51Ng2MSSEmpoDyKWM2BxmouL6cikMOoK6uPshdHZcCCoJsbpP3Up4SfqO8AOwc67SpLQR76s5IHF4T4fysxU9aRQC007cEnKnoF"
+
 const stripePromise = loadStripe(PUBLIC_KEY);
 
 
