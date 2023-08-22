f3779f4eef7d0b97ba46ecb6961d3a214878a8f3
Rohit Gupta <124511104+Rohit171020@users.noreply.github.com>
2023-08-22 05:36:25 +0530

    Dashboard.js

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -1,35 +1,38 @@
 import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { AppContext } from "../../utils/AppContext";
 import PlanCard from "../plan-card/PlanCard";
+import { AppContext } from "../../utils/AppContext";
 import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
   const { logout, user, fetchAllSubscriptions, transactions } = useContext(AppContext);
   const navigator = useNavigate();
 
+
   
   useEffect(() => {
     fetchAllSubscriptions();
-  }, []); 
+  }, []);
+
   const handleLogout = () => {
     localStorage.removeItem("rememberedUser");
     navigator("/");
-    
   };
 
   return (
     <div style={{ width: "100%" }}>
-      <h1 style={{ color: "white", textAlign: "center" }}> Welcome {user && user.name}</h1>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Welcome {user ? user.name : ""}
+      </h1>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <button
-          onClick={handleLogout} 
+          onClick={handleLogout}
           className="btn"
           style={{
             backgroundColor: "white",
             color: "#1e4c91",
             width: "inherit",
-            margin: "10px",
+            margin: "13px",
           }}
         >
           Log Out
@@ -47,20 +50,29 @@ export default function Dashboard() {
         </Link>
       </div>
 
-      <div style={{ marginTop: 20 }}>
+      <div style={{ marginTop: 22 }}>
         {transactions ? (
-          <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", gap: "20px" }}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              flexWrap: "wrap",
+              gap: "23px",
+            }}
+          >
             {transactions.map((item) => (
               <PlanCard
-                subId={item.subscriptionId}
                 key={item._id}
+                subId={item.subscriptionId}
                 period={item.planDetails.period}
                 type={item.active ? "active" : null}
                 plan={item.planDetails.plan}
                 amount={
-                  item.planDetails.period === "month"
-                    ? item.planDetails.planInfo["Monthly-Price"]
-                    : item.planDetails.planInfo["Yearly-Price"]
+                  item.planDetails.planInfo[
+                    item.planDetails.period === "month"
+                      ? "Monthly-Price"
+                      : "Yearly-Price"
+                  ]
                 }
                 data={item}
               />
